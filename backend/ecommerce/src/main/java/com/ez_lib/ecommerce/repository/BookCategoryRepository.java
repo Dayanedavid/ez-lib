a9dd9eb0d7e1a405a5d956006cfe3995347ed297
Amanda Leite <mandirleite@gmail.com>
2024-10-27 12:37:14 -0300

    corrigindo back-end URLs

--- backend/ecommerce/src/main/java/com/ez_lib/ecommerce/repository/BookCategoryRepository.java
+++ backend/ecommerce/src/main/java/com/ez_lib/ecommerce/repository/BookCategoryRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-@CrossOrigin("https://ez-lib.onrender.com/books")
+@CrossOrigin("https://ez-lib.onrender.com")
 @RepositoryRestResource(collectionResourceRel = "bookCategory", path = "book-category")
 public interface BookCategoryRepository extends JpaRepository<BookCategory, Long> {
 }
--- backend/ecommerce/src/main/java/com/ez_lib/ecommerce/repository/BookRepository.java
+++ backend/ecommerce/src/main/java/com/ez_lib/ecommerce/repository/BookRepository.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.Optional;
 
-@CrossOrigin("https://ez-lib.onrender.com/books")
+@CrossOrigin("https://ez-lib.onrender.com")
 public interface BookRepository extends JpaRepository<Book, Long> {
     Page<Book> findByCategoryId(@Param("id") Long id, Pageable pageable);
 
